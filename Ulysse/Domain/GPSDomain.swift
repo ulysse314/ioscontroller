4149797b79ff3038eb1287438d22f77ab55ab8cb
ulysse314 <ulysse314>
2019-12-30 05:14:54 +0100

    Adding mode in the first value of the GPS button

--- Ulysse/Domain/GPSDomain.swift
+++ Ulysse/Domain/GPSDomain.swift
@@ -8,6 +8,21 @@ class GPSDomain: ModuleDomain {
 
   // MARK: - Short values
 
+  override func value1() -> String? {
+    let module: Module? = self.module(name: "gps")
+    let modeValue: Int = module?.value(key: "mode") as? Int ?? 0
+    switch modeValue {
+    case 1:
+      return "-"
+    case 2:
+      return "2D"
+    case 3:
+      return "3D"
+    default:
+      return "?"
+    }
+  }
+
   override func value2() -> String? {
     let module: Module? = self.module(name: "gps")
     let satCount: Int = module?.value(key: "sat") as? Int ?? 0
